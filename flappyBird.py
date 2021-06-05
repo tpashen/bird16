eef7aec8310e949f09bd54dcf781d2c2b37265e9
Yasha367 <82272991+Yasha367@users.noreply.github.com>
2021-06-05 16:27:24 +0300

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -11,3 +11,17 @@ mw.fill(back)
 clock = pygame.time.Clock()
  
 game = True
+class Area():
+    def __init__(self, x=0, y=0, widht=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, widht, height)
+        self.fill_color = back
+        if color:
+            self.fill_color = color
+    def color(self, new_color):
+        self.fill_color = new_color
+    def fill(self):
+        pygame.draw.rect(mw, self.fill_color, self.rect)
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+    def collidepoint(self, rect):
+        return self.rect.collidepoint(rect)
